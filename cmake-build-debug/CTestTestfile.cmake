eb33185f64f717eae00b214403d846b3b8a50a8f
Hussein Soliman <127189589+HusseinnSolimann@users.noreply.github.com>
2023-12-12 22:20:30 +0100

    done

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[game_tests]=] "/Users/husseinsoliman/Documents/GitHub/2D-game/cmake-build-debug/game_tests")
-set_tests_properties([=[game_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/husseinsoliman/Documents/GitHub/2D-game/CMakeLists.txt;132;add_test;/Users/husseinsoliman/Documents/GitHub/2D-game/CMakeLists.txt;0;")
+set_tests_properties([=[game_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/husseinsoliman/Documents/GitHub/2D-game/CMakeLists.txt;53;add_test;/Users/husseinsoliman/Documents/GitHub/2D-game/CMakeLists.txt;0;")
 subdirs("_deps/googletest-build")
